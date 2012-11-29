1098297c97629a4f86fabb5e0db100e4e3bafbab
Josh Matthews <josh@joshmatthews.net>
2012-11-29 12:13:23 -0500

    Add first commit date data to the database.

--- swagger.py
+++ swagger.py
@@ -1,22 +1,22 @@
+import sys
 import sqlite3
 from git import *
-import sys
 from collections import defaultdict
 
 try:
     f = open('last_commit', 'rb')
     last_commit = f.read()
     f.close()
-except e:
+except:
     last_commit = None
 
 repo = Repo(sys.argv[1])
 assert repo.bare == False
 
-commit_info = defaultdict(lambda: {'name': '', 'commits': 0, 'last_commit': 0})
+commit_info = defaultdict(lambda: {'name': '', 'commits': 0, 'first_commit': sys.maxint, 'last_commit': 0})
 
 for commit in repo.iter_commits('master'):
-    if commit.hexsha in last_commit:
+    if last_commit and commit.hexsha in last_commit:
         break
     author = commit.author
     commits = commit_info[author.email]['commits']
@@ -25,6 +25,8 @@ for commit in repo.iter_commits('master'):
         commit_info[author.email]['name'] = author.name
     if commit.committed_date > commit_info[author.email]['last_commit']:
         commit_info[author.email]['last_commit'] = commit.committed_date
+    if commit.committed_date < commit_info[author.email]['first_commit']:
+        commit_info[author.email]['first_commit'] = commit.committed_date
 
 conn = sqlite3.connect('committers.db')
 c = conn.cursor()
@@ -32,14 +34,14 @@ c = conn.cursor()
 if not last_commit:
     c.execute('''DROP TABLE IF EXISTS committers''')
     c.execute('''CREATE TABLE committers
-                 (name text, email text, commits integer, last_commit datetime)''')
+                 (name text, email text, commits integer, first_commit datetime, last_commit datetime)''')
 
     records = []
     for email in commit_info:
         info = commit_info[email]
-        records.append((info['name'], email, info['commits'], info['last_commit']))
+        records.append((info['name'], email, info['commits'], info['first_commit'], info['last_commit']))
 
-    c.executemany('INSERT INTO committers VALUES (?, ?, ?, ?)', records)
+    c.executemany('INSERT INTO committers VALUES (?, ?, ?, ?, ?)', records)
 
 else:
     for email in commit_info:
@@ -49,9 +51,9 @@ else:
             c.execute('UPDATE committers SET commits=?, last_commit=? WHERE email=?',
                       (int(res[0]) + commit_info[email]['commits'], commit_info[email]['last_commit'], email))
         else:
-            c.execute('INSERT INTO committers VALUES (?, ?, ?, ?)',
+            c.execute('INSERT INTO committers VALUES (?, ?, ?, ?, ?)',
                       (commit_info[email]['name'], email, commit_info[email]['commits'],
-                       commit_info[email]['last_commit']))
+                       commit_info[email]['first_commit'], commit_info[email]['last_commit']))
             print email
 
 conn.commit()
